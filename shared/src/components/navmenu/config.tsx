f3ef19bfd0223645f5c0a125a96cd5e11f2c74b8
aidanCQ <aidan.keay@quantinuum.com>
2024-06-20 15:14:44 +0100

    Add optional external key.

--- shared/src/components/navmenu/config.tsx
+++ shared/src/components/navmenu/config.tsx
@@ -4,12 +4,15 @@ import { z } from 'zod';
 
 const linkSchema = z.object({
     href: z.string(),
-    title: z.string()
+    title: z.string(),
+    pathMatch: z.string(),
+    external: z.boolean().optional().default(false),
 });
 export const navConfigSchema = z.object({
     navTextLinks: z.array(linkSchema),
     navIconLinks: z.array(z.intersection(linkSchema, z.object({iconImageURL: z.string()}))),
     navProductName: z.string()
+
 })
 
 export const navConfig = navConfigSchema.parse({
