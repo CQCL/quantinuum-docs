e518778d8afccb3495f25f20de0206ca52b8f106
Irfan Khan <irfan.khan@quantinuum.com>
2024-09-13 09:19:36 -0300

    add link to navbar

--- landing_page/src/app/components/navmenu/config.tsx
+++ landing_page/src/app/components/navmenu/config.tsx
@@ -13,7 +13,33 @@ type NavItem = {
   }[]
 }
 
-export const links = [] satisfies NavItem[]
+export const links = [
+  {
+    title: 'H-Series',
+    href: 'https://docs.quantinuum.com/h-series',
+    pathMatch: '/h-series',
+  },
+  {
+    title: 'Nexus',
+    href: 'https://docs.quantinuum.com/nexus',
+    pathMatch: '/nexus',
+  },
+  {
+    title: 'TKET',
+    href: 'https://docs.quantinuum.com/tket',
+    pathMatch: '/tket',
+  },
+  {
+    title: 'InQuanto',
+    href: 'https://docs.quantinuum.com/inquanto',
+    pathMatch: '/inquanto',
+  },
+  {
+    title: 'lambeq',
+    href: 'https://docs.quantinuum.com/lambeq',
+    pathMatch: '/lambeq',
+  },
+] satisfies NavItem[]
 
 export type ActivePaths = (typeof links)[number]['href']
 const defaultLink = (props: ComponentProps<'a'>) => <a {...props}></a>
