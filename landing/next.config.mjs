eaed725b4abb559f43394d2619dc52a80e689704
aidanCQ <aidan.keay@quantinuum.com>
2024-09-20 12:15:56 +0100

    Remove base path.

--- landing/next.config.mjs
+++ landing/next.config.mjs
@@ -1,13 +1,4 @@
-import { z } from "zod";
-
-export const runtimeENVs = z.object({
-  BASE_PATH: z.string(),
-});
-
-const values = runtimeENVs.parse(process.env);
 export default {
   output: "export",
   images: { unoptimized: true },
-  assetPrefix: values.BASE_PATH + "/",
-  basePath: values.BASE_PATH,
 };
