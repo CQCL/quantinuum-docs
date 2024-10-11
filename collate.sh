c1a378e9596e15bda59e79c7e31eafb2e73556ca
Irfan Khan <irfan.khan@quantinuum.com>
2024-10-11 10:55:14 +0100

    build script to pull html assets into _build directory

--- collate.sh
+++ collate.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+if [ -d _build ]; then
+  rm -rf _build
+fi
+
+mkdir _build
+
+cp -rf base_site/* _build/.
+
 echo "H-Series"
-gh release download -p hseries-docs-*  -R CQCL/hseries-documentation -D base_site/
-tar -xzf base_site/hseries-docs-*.tar.gz -C base_site/h-series
+gh release download -p hseries-docs-*  -R CQCL/hseries-documentation -D _build/
+tar -xzf _build/hseries-docs-*.tar.gz -C _build/h-series
 
 echo "Nexus"
-gh release download -p nexus-docs-*.tar.gz -R CQCL-DEV/nexus-docs -D base_site/ 
-tar -xzf base_site/nexus-docs-*.tar.gz -C base_site/nexus
+gh release download -p nexus-docs-*.tar.gz -R CQCL-DEV/nexus-docs -D _build/ 
+tar -xzf _build/nexus-docs-*.tar.gz -C _build/nexus
 
 echo "TKET"
-gh release download -p tket-docs-* -R CQCL-DEV/tket-site -D base_site/
-tar -xzf base_site/tket-docs-*.tar.gz -C base_site/tket
+gh release download -p tket-docs-* -R CQCL-DEV/tket-site -D _build/
+tar -xzf _build/tket-docs-*.tar.gz -C _build/tket
 
 echo "InQuanto"
-gh release download -p inquanto-docs-* -R CQCL-DEV/inquanto-docs -D base_site/
-tar -xzf base_site/inquanto-docs-*.tar.gz -C base_site/inquanto
+gh release download -p inquanto-docs-* -R CQCL-DEV/inquanto-docs -D _build/
+tar -xzf _build/inquanto-docs-*.tar.gz -C _build/inquanto
 
 echo "Lambeq"
-gh release download -p lambeq-docs-* -R CQCL/lambeq-docs -D base_site/
-tar -xzf base_site/lambeq-docs-*.tar.gz -C base_site/lambeq
+gh release download -p lambeq-docs-* -R CQCL/lambeq-docs -D _build/
+tar -xzf _build/lambeq-docs-*.tar.gz -C _build/lambeq
